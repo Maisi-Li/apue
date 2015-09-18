9a23e4bda3eff7f60dc8ea026aa0d6049b38230c
miceteat <limaisi221@gmail.com>
2015-09-18 18:12:01 -0400

    check two path is valid.

--- hw2/tcp.c
+++ hw2/tcp.c
@@ -5,13 +5,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #define BUFFER_SIZE 4096
 
 int main(int argc, char* argv[]) {
-	int inputFd, outputFd, outputDir;
+	int inputFd, outputFd;
+	DIR *outputDir;
 	ssize_t readIn, WriteOut;
 	char buffer[BUFFER_SIZE];
-
+	struct stat stat1, stat2;
 	if(argc != 3) {
 		fprintf(stderr, "tcp: Usage: %s filename filename\n",argv[0]);
 		exit(1);
@@ -20,10 +23,26 @@ int main(int argc, char* argv[]) {
 	// Create input file descroptor
 	inputFd = open(argv[1],O_RDONLY);
 	if(inputFd == -1) {
-		fprintf(stderr,"tcp: can not open file1: %s\n",
-				strerror(errno));
+		fprintf(stderr,"%s: %s: %s\n",
+				argv[0], argv[1], strerror(errno));
 		exit(2);
 	}
+	if(stat(argv[1],&stat1) == -1) {
+		fprintf(stderr, "%s: stat: %s",argv[0], strerror(errno));
+	}
+	printf("stat1.inode = %d\n", (int)stat1.st_ino);
 
-	return(2);
+	// Check if the second argument is a file or a Directory 	
+	if(stat(argv[2],&stat2) == -1)	{
+		fprintf(stderr,"%s: %s: %s",argv[0], argv[2],strerror(errno));
+		exit(3);
+	}
+	
+	if(S_ISDIR(stat2.st_mode)) 
+		outputDir = opendir(argv[2]);	
+	else
+		outputFd = open(argv[2],O_WRONLY);
+	if(outputFd != -1)
+		printf("stat2.inode = %d\n", (int)stat2.st_ino);
+	return(0);
 }
--- hw2/test_tcp.sh
+++ hw2/test_tcp.sh
@@ -3,7 +3,21 @@ cc -Wall tcp.c
 
 #1. Three arguement
 echo "1.three arguemnts"
-./a.out ddd aaa ddd
+echo "./a.out dd dd ddd"
+./a.out dd dd ddd
+echo \\n
 
-echo "1.Test if the first argument is not a file"
+echo "2.Test if the first argument is not a file"
+echo "tcp dd dd"
 ./a.out dd ddd
+echo \\n
+
+echo "3.Test if the second argument is not a file or directory"
+echo "tcp ./temp/1.txt ddd"
+./a.out ./temp/1.txt ddd
+echo \\n
+
+echo "4.two arguments are the same file"
+echo "./a.out ./temp/1.txt ./temp/1.txt"
+./a.out ./temp/1.txt ./temp/1.txt
+echo \\n
