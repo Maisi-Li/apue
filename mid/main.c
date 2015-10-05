7d6c707a9f48f988a5c1c322e059d56c8af12fb3
miceteat <limaisi221@gmail.com>
2015-10-05 11:56:34 -0400

    snapshot create

--- mid/main.c
+++ mid/main.c
@@ -2,7 +2,7 @@
 #include <sys/stat.h>
 #include <sys/dir.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 #define	MAXFILES	1024
 
 struct fbuf {
@@ -16,15 +16,12 @@ struct fbuf {
 	long  f_size;
 	long  f_time;
 	
-}
+};
 int	aflg, Aflg, Cflg, cflg, dflg, Fflg, fflg, hflg, iflg, kflg, lflg, nflg;
-int 	qflg, Rflg, rflg, Sflg, sflg, tflg, uflg, uflg, wflg, xflg, oneflg;
+int 	qflg, Rflg, rflg, Sflg, sflg, tflg, uflg, wflg, xflg, oneflg;
 char	*curPath = ".";
-int main() {
+int main(int argc,char *argv[]) {
 	
-	if(--argc > 0 && *argv[1] == '-') {
-		argv++;
-		while(*++*argv) switch (**argv)	{
 		
 		case 'a':
 			aflg++;
@@ -41,22 +38,16 @@ int main() {
 		default:
 			continue;
 		}
-		
-	if(fflg) {
-		aflg++;
-		lflg = 0;
-		sflg = 0;
-		tflg = 0;
-		
-	}
+	}	
 
 	if(argc == 0) {
 		argc++;
 		argv = &curPath - 1;
 	}
-
 	
-	}	
+	
+	
+		
 return(0);
 }
 
